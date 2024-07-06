efbeb1dd6055e1b0c343084d174b8a80fa918850
John Burbridge <jburbridge@clari.com>
2024-07-06 10:20:27 -0700

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ ! -z $(which brew) ];
+  then
+  homebrew_prefix=$(brew --prefix)
+  else
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+echo $homebrew_prefix
+
+brew install font-jetbrains-mono-nerd-font
+brew install starship
